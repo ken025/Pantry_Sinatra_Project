a82a5843b74eb760ab00cc3b469d78b2dd7c1b42
ken <kennethmanuel250@gmail.com>
2020-10-09 15:36:31 -0400

    uodated signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,83 +1,28 @@
 class UsersController < ApplicationController 
 
-#     get '/users/signup' do
-#         erb :"users/signup"
-#     end
-
-#     post '/users' do
-#         #create user
-#         @user = User.new(params)
-#         # save
-#         if @user && @user.save
-#         # if it saves
-#             session[:user_id] = @user.id
-#             # set session ID
-#             redirect "/items"
-#             # redirect the user
-#         else
-#         # if it doesn't save
-#         # binding.pry
-#             erb :"users/signup"
-#             #redirect back to signup
-#         end
-#     end
-# end
-
-#     get '/signup' do
-
-#         erb :"/signup"
-#     end
-
-#     post '/signup' do
-#         if 
-#           @user = User.create(email: params[:email], password: params[:password])
-#           session[:user_id] = @user.id
-          
-#           redirect "/items"
-#         else  
-          
-#           redirect "/signup"
-#         end
-#       end
-
-#     post '/users' do
-#         #create user
-#         @user = User.new(params)
-#         # save
-#         if @user && @user.save
-#         # if it saves
-#             session[:user_id] = @user.id
-#             # set session ID
-#             redirect "/items"
-#             # redirect the user
-#         else
-#         # if it doesn't save
-#         # binding.pry
-#             erb :"users/signup"
-#             #redirect back to signup
-#         end
-#     end
-# end 
-
 get '/signup' do
-    # redirect_if_logged_in
+    redirect_if_logged_in
     erb :'users/signup'
   end
 
   # CREATE -- accept sign up params and create a user
-  post "/users" do
-    find_user_by_email
+  post "/signup" do
+    @user = User.find_by(username: params[:username])
     if @user
+      flash[:alert] = "This username has been taken."
+
       redirect "/signup"
     else
-      @user = User.new(params[:user])
+
+      @user = User.new(email: params[:email], username: params[:username], password: params[:password])
       if @user.save
         session[:user_id] = @user.id
         redirect "/items"
 
       else
+
         redirect "/signup"
       end 
     end 
-end 
-end 
+    end 
+  end 

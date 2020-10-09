d7180a7a060442b70da2fa156e1628886e001b3a
ken <kennethmanuel250@gmail.com>
2020-10-09 16:36:46 -0400

    migrated routes in sessions to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,40 @@ get '/signup' do
 
         redirect "/signup"
       end 
-    end 
-    end 
+    end
   end 
+    
+    get '/login' do
+
+      erb :"users/login"
+  end
+
+  post '/login' do
+      #binding.pry
+      #finds the user
+      user = User.find_by(username: params[:username])
+
+      #authenticates the user (from decrypt gem)
+      if user && user.authenticate(params[:password])
+
+      # logs the user in and redirect
+          session[:user_id] = user.id
+
+          redirect "/items"
+      else
+        flash[:alert] = "Sorry, the email and/or password you entered was incorrect. Please try again."
+          redirect "/login"
+      end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      
+      erb :'/welcome'
+    else
+      
+      redirect '/'
+    end
+  end
+end
